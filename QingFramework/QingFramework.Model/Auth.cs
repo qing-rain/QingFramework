2393468de03d3a6c84dbe6f1c3422c12cf6730e7
qingrain <353752459@qq.com>
2020-09-15 11:06:35 +0800

    Create Model

---
+++ QingFramework/QingFramework.Model/Auth.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QingFramework.Model
+{
+    public class Auth
+    {
+    }
+}
--- QingFramework/QingFramework.Web/Program.cs
+++ QingFramework/QingFramework.Web/Program.cs
@@ -20,5 +20,6 @@ namespace QingFramework.Web
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+       
     }
 }
